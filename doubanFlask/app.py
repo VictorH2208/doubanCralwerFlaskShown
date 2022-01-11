228f3aad7205b8e7b0b87aabff3957f06f144d15
VictorH2208 <60560735+VictorH2208@users.noreply.github.com>
2022-01-11 00:49:25 -0500

    Jan 11 Version

---
+++ doubanFlask/app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template
+import sqlite3
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template("index.html")
+
+@app.route('/index')
+def home():
+    #return render_template("index.html")
+    return index()
+
+@app.route('/movie')
+def movie():
+    datalist = []
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movieTop250"
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+    cur.close()
+    con.close()
+    return render_template("movie.html", movies= datalist)
+
+@app.route('/score')
+def score():
+    return render_template("score.html")
+
+@app.route('/word')
+def word():
+    return render_template("word.html")
+
+@app.route('/team')
+def team():
+    return render_template("team.html")
+
+if __name__ == '__main__':
+    app.run()
